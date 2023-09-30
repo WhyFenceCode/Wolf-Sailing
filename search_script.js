cdc6c35e1e1957a12b49440f7d31371a2c625e95
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2023-09-30 10:36:41 -0700

    Update search_script.js

--- search_script.js
+++ search_script.js
@@ -1,7 +1,25 @@
-$('.Search').hover(
-  function(){ // Mouse over
-    $('.Actions').css('grid-template-columns', '5fr 0fr 0fr');
-  },
-  function(){ // Mouse out
-    $('.Actions').css('grid-template-columns', ''); // Reset to original
+$(document).ready(function() {
+    function checkWidth() {
+        var windowSize = window.matchMedia('(min-width: 768px)');
+
+        if (windowSize.matches) {
+            // If media query matches
+            $('.Search').hover(
+                function(){ // Mouse over
+                    $('.Actions').css('grid-template-columns', '5fr 0fr 0fr');
+                },
+                function(){ // Mouse out
+                    $('.Actions').css('grid-template-columns', ''); // Reset to original
+                }
+            );
+        } else {
+            // Remove hover effects for smaller screens
+            $('.Search').off('mouseenter mouseleave');
+        }
+    }
+
+    // Execute on load
+    checkWidth();
+    // Bind event listener
+    $(window).resize(checkWidth);
 });
