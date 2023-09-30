99ad832204354023b7df8a7d278d2225ac607529
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2023-09-30 10:17:10 -0700

    Update search_script.js

--- search_script.js
+++ search_script.js
@@ -1,1 +1,7 @@
-document.addEventListener("touchstart", function(){}, true);
+$('.Search').hover(
+  function(){ // Mouse over
+    $('.Actions').css('grid-template-columns', '1fr 0fr 0fr');
+  },
+  function(){ // Mouse out
+    $('.Actions').css('grid-template-columns', ''); // Reset to original
+});
