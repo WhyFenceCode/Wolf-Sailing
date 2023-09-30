9abfc8b4bb214be7a29f6e4740bc5fd102dd171e
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2023-09-30 10:48:20 -0700

    Update search_script.js

--- search_script.js
+++ search_script.js
@@ -6,10 +6,12 @@ $(document).ready(function() {
             // If media query matches
             $('.Search').hover(
                 function(){ // Mouse over
-                    $('.Actions').css('grid-template-columns', '5fr 0fr 0fr');
+                    $('.Actions').css('grid-template-columns', '1fr 0fr 0fr');
+                    $('.Bag, .Accounts').css('opacity', '0'); // Make other grid items invisible
                 },
                 function(){ // Mouse out
                     $('.Actions').css('grid-template-columns', ''); // Reset to original
+                    $('.Bag, .Accounts').css('opacity', '1'); // Make other grid items visible
                 }
             );
         } else {
