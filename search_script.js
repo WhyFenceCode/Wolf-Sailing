0e7c50b8a1d81b759a16af88929344da3c1f7e82
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2023-10-03 08:15:44 -0700

    Update search_script.js

--- search_script.js
+++ search_script.js
@@ -7,17 +7,14 @@ $(document).ready(function() {
             $('.Search').hover(
                 function(){ // Mouse over
                     $('.Actions').css('grid-template-columns', '1fr 0fr 0fr');
-                    $('.Bag, .Account, .Searchicon').css('opacity', '0'); // Make other grid items invisible
+                    $('.Bag, .Account').css('opacity', '0'); // Make other grid items invisible
                     setTimeout(function() {
                     $('.Bag, .Account').css('display', 'none');
-                    $('.Searchbar).css('display', 'block');
-                    }, 600);
                 },
                 function(){ // Mouse out
                     $('.Bag, .Account').css('display', ''); // Reset to original
                     $('.Actions').css('grid-template-columns', ''); // Reset to original
-                    $('.Bag, .Account, .Searchicon').css('opacity', '1'); // Make other grid items visible
-                    $('.Searchbar).css('display', 'none');
+                    $('.Bag, .Account').css('opacity', '1'); // Make other grid items visible
                 }
             );
         } else {
