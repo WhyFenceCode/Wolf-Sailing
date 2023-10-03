16fbdc7c7b92bce32a4ab2c212b4094ff21b1b0f
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2023-10-03 13:14:59 -0700

    Update acctions_script.js

--- acctions_script.js
+++ acctions_script.js
@@ -1,11 +1,11 @@
 function searchFunction() {
-  
+  console.error("Search");
 }
 
 function bagFunction() {
-  
+  console.error("Bag");
 }
 
 function accountFunction() {
-  
+  console.error("Account");
 }
