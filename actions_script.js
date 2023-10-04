9ed4dad9a0a6100a0ef24e1b3e89ef10e4cbd146
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2023-10-04 11:53:19 -0700

    Update actions_script.js

--- actions_script.js
+++ actions_script.js
@@ -1,9 +1,15 @@
-function searchFunction() {
+function openSearchFunction() {
   console.error("Search");
+  searchUp.classList.add("show");
+}
+
+function closeSearchFunction(){
+  myPopup.classList.remove("show");
 }
 
 function bagFunction() {
   console.error("Bag");
+  window.location = "bag.html"
 }
 
 function accountFunction() {
