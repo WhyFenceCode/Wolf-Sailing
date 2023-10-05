dd4b293958a065949c190fef6f00579a7d349d2c
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2023-10-04 17:24:05 -0700

    Update products.js

--- products/products.js
+++ products/products.js
@@ -2,5 +2,6 @@ function main(){
   const urlParams = new URLSearchParams(window.location.search);
   const sid = urlParams.get('sid');
 
-  
+  import data from './'+ sid +'.json?type=module';
+  console.log(data);  
 }
