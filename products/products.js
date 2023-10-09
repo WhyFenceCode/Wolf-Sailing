6cab27ae670d3c763398126f5e024d27e4c3b13e
WhyFenceCode <120431739+WhyFenceCode@users.noreply.github.com>
2023-10-09 10:18:03 -0700

    Update products.js

--- products/products.js
+++ products/products.js
@@ -9,45 +9,74 @@ function main(){
   var feat = obj.pids.fields;
 
   if (feat.length > 0) {
+    addDiv("content", "sidcol")
+    
     if (feat.length > 0) {
+      addDiv("sidcol", "featcol")
       feat.forEach(featFunc);
     }
   
     if (type.length > 0) {
+      addDiv("sidcol", "typecol")
       type.forEach(typeFunc);
     }
   
     if (seri.length > 0) {
+      addDiv("sidcol", "sericol")
       seri.forEach(seriFunc);
     }
   } else if (type.length > 0) {
+    addDiv("content", "sidcol")
+    
     if (feat.length > 0) {
+      addDiv("sidcol", "featcol")
       feat.forEach(featFunc);
     }
   
     if (type.length > 0) {
+      addDiv("sidcol", "typecol")
       type.forEach(typeFunc);
     }
   
     if (seri.length > 0) {
+      addDiv("sidcol", "sericol")
       seri.forEach(seriFunc);
     }
   } else if (seri.length > 0) {
+    addDiv("content", "sidcol")
+    
     if (feat.length > 0) {
+      addDiv("sidcol", "featcol")
       feat.forEach(featFunc);
     }
   
     if (type.length > 0) {
+      addDiv("sidcol", "typecol")
       type.forEach(typeFunc);
     }
   
     if (seri.length > 0) {
+      addDiv("sidcol", "sericol")
       seri.forEach(seriFunc);
     }
   }
 
   pids.forEach(addProduct);
   
+}
+
+function addDiv(parent, name){
+  // Get the parent element by class
+  var parent = document.querySelector("." + parent);
+  
+  // Create a new div element
+  var child = document.createElement("div");
+  
+  // Set the class attribute of the child element
+  child.setAttribute("class", name);
+  
+  // Append the child element to the parent element
+  parent.appendChild(child);
 }
 
 function featFunc(item, index){
